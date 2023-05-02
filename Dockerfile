045c987858305d4056059a43bcfcc6c747269bb8
pedro vinicius <pedrovinicius.dev@gmail.com>
2023-05-02 12:45:19 -0300

    First commit

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM node
+
+WORKDIR /usr/app
+
+COPY package.json ./
+
+RUN npm install
+
+EXPOSE 3333
+
+CMD [ "npm", "run", "dev" ]
---
+++ src/app.ts
@@ -0,0 +1,5 @@
+import { app } from "./server";
+
+const port = 3333;
+
+app.listen(port, () => console.log(`Server is runing in port ${port}`));
