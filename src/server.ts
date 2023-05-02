dc27ceea5946f1ae04e124028efb5cc3b9b5f155
pedro vinicius <pedrovinicius.dev@gmail.com>
2023-05-02 13:34:59 -0300

    feat: Manipulando erros

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import "express-async-errors";
+import { errorHandler } from "./middlewares/errorHandler";
 
 const app = express();
 
 app.use(express.json());
+app.use(errorHandler);
 
 export { app };
